b0cf5181cbee660533748974d4a70a255ef17665
gabriel-ferreira-da-silva <gab_fs@outlook.co>
2025-05-30 14:01:25 -0300

    URL mudada nas request e no backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ['http://localhost:5173','https://task-sphere-eight.vercel.app'],
+    origin: 'https://task-sphere-eight.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
-
